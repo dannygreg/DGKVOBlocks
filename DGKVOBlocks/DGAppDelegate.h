f6fc15398d65cf4510755328af993e12f5ff6b2e
Danny Greg <danny@dannygreg.com>
2011-10-02 11:16:13 +0100

    Initial Commit

---
+++ DGKVOBlocks/DGAppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  DGAppDelegate.h
+//  DGKVOBlocks
+//
+//  Created by Danny Greg on 02/10/2011.
+//  Copyright (c) 2011 No Thirst Software. All rights reserved.
+//
+
+#import <Cocoa/Cocoa.h>
+
+@interface DGAppDelegate : NSObject <NSApplicationDelegate>
+
+@property (assign) IBOutlet NSWindow *window;
+
+@end
---
+++ DGKVOBlocks/DGAppDelegate.m
@@ -0,0 +1,20 @@
+//
+//  DGAppDelegate.m
+//  DGKVOBlocks
+//
+//  Created by Danny Greg on 02/10/2011.
+//  Copyright (c) 2011 No Thirst Software. All rights reserved.
+//
+
+#import "DGAppDelegate.h"
+
+@implementation DGAppDelegate
+
+@synthesize window = _window;
+
+- (void)applicationDidFinishLaunching:(NSNotification *)aNotification
+{
+    // Insert code here to initialize your application
+}
+
+@end
