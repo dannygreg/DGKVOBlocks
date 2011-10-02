13dd093b6eff249d2cb307862c65b7dc55179660
Abizer Nasir <abizern@abizern.org>
2011-10-02 19:14:28 +0100

    Use a more graphical UI as an example project
    This intentionally uses a bad algorithm to demonstrate blocking the UI.

--- DGKVOBlocks/DGAppDelegate.m
+++ DGKVOBlocks/DGAppDelegate.m
@@ -9,15 +9,42 @@
 #import "DGAppDelegate.h"
 #import "NSObject+DGKVOBlocks.h"
 
+@interface DGAppDelegate () {
+@private
+    id stringObserver;
+    id numberObserver;
+}
+
+- (NSUInteger)fibonacciNumber:(NSUInteger)aNumber;
+@end
+
 @implementation DGAppDelegate
 
-@synthesize window = _window;
+@synthesize window =_window;
 @synthesize string;
+@synthesize fibonacciField;
+@synthesize number;
+
+- (id)init 
+{
+    if (!(self = [super init])) {
+        return nil;
+    }
+    number = 1;
+    
+    return self;
+}
+
+- (void)dealloc {
+    [self dgkvo_removeObserverWithIdentifier:numberObserver];
+}
+
 
 - (void)applicationDidFinishLaunching:(NSNotification *)aNotification
 {
-    
-	id identifier = [self dgkvo_addObserverForKeyPath:@"string" options:NSKeyValueObservingOptionNew block:^(NSDictionary *change) {
+    number = 1;
+	
+    stringObserver = [self dgkvo_addObserverForKeyPath:@"string" options:NSKeyValueObservingOptionNew block:^(NSDictionary *change) {
 		NSLog(@"%@", change);
 	}];
 	
@@ -25,9 +52,25 @@
 	self.string = @"TWO";
 	self.string = @"THREE";
 	
-	[self dgkvo_removeObserverWithIdentifier:identifier];
+	[self dgkvo_removeObserverWithIdentifier:stringObserver];
     
     self.string = @"This should not appear.";
+    
+    numberObserver = [self dgkvo_addObserverForKeyPath:@"number" options:NSKeyValueObservingOptionNew block:^(NSDictionary *change) {
+        NSLog(@"%@", change);
+        self.fibonacciField.integerValue = [self fibonacciNumber:number];
+        
+	}];
+    
+}
+
+
+// Don't laugh. I know it's terrible - that's the whole point.
+- (NSUInteger)fibonacciNumber:(NSUInteger)aNumber {
+    if (aNumber < 2) {
+        return aNumber;
+    }
+    return [self fibonacciNumber:aNumber - 2] + [self fibonacciNumber:aNumber -1];
 }
 
 @end
