e94cd0ed69b00a7deb181d132753f40955c1192f
Danny Greg <danny@dannygreg.com>
2011-10-02 20:52:37 +0100

    Outline the new API.

--- DGKVOBlocks/NSObject+DGKVOBlocks.h
+++ DGKVOBlocks/NSObject+DGKVOBlocks.h
@@ -30,7 +30,7 @@ typedef void (^DGKVOObserverBlock)(NSDictionary *change);
 
 @interface NSObject (DGKVOBlocks)
 
-- (id)dgkvo_addObserverForKeyPath:(NSString *)keyPath options:(NSKeyValueObservingOptions)options block:(DGKVOObserverBlock)block;
+- (id)dgkvo_addObserverForKeyPath:(NSString *)keyPath options:(NSKeyValueObservingOptions)options queue:(NSOperationQueue *)queue block:(DGKVOObserverBlock)block;
 - (void)dgkvo_removeObserverWithIdentifier:(id)identifier;
 
 @end
