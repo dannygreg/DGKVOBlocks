3d370db1a9e510193a42ad8eb356b3ef61bb0ad2
Danny Greg <danny@dannygreg.com>
2011-10-02 13:22:54 +0100

    Adding the works.

---
+++ DGKVOBlocks/NSObject+DGKVOBlocks.h
@@ -0,0 +1,15 @@
+//
+//  NSObject+DGKVOBlocks.h
+//  DGKVOBlocks
+//
+//  Created by Danny Greg on 02/10/2011.
+//  Copyright (c) 2011 No Thirst Software. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+typedef void (^DGKVOObserverBlock)(NSDictionary *change);
+
+@interface NSObject (DGKVOBlocks)
+
+@end
---
+++ DGKVOBlocks/NSObject+DGKVOBlocks.m
@@ -0,0 +1,25 @@
+//
+//  NSObject+DGKVOBlocks.m
+//  DGKVOBlocks
+//
+//  Created by Danny Greg on 02/10/2011.
+//  Copyright (c) 2011 No Thirst Software. All rights reserved.
+//
+
+#import "NSObject+DGKVOBlocks.h"
+
+@interface DGKVOBlocksObserver : NSObject 
+
+//@property (nonatomic, copy) 
+
+@end
+
+@implementation DGKVOBlocksObserver
+
+<#methods#>
+
+@end
+
+@implementation NSObject (DGKVOBlocks)
+
+@end
