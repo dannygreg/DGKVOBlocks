7aeee00c4b80896a6f43dc5a229eaa7cbffdba77
Mike Abdullah <me@mikeabdullah.net>
2011-10-03 14:11:16 +0100

    Implement -dealloc for DGKVOBlocksObserver so ivars don't get leaked under manual reference counting.

--- DGKVOBlocks/NSObject+DGKVOBlocks.m
+++ DGKVOBlocks/NSObject+DGKVOBlocks.m
@@ -54,6 +54,17 @@ NSString *const DGKVOBlocksObserversAssociatedObjectsKey = @"DGKVOBlocksObserver
 @synthesize keyPath = _keyPath;
 @synthesize queue = _queue;
 
+#if !__has_feature(objc_arc)
+- (void)dealloc
+{
+    [_keyPath release];
+    [_queue release];
+    [_block release];
+    
+    [super dealloc];
+}
+#endif
+
 - (void)observeValueForKeyPath:(NSString *)keyPath ofObject:(id)object change:(NSDictionary *)change context:(void *)context 
 {
     if (context == &DGKVOBlocksObservationContext) {
